de9996420c37bd99ac290de9e94e0bfc09ea9ebf
Bence Bardosi <bardosi.bence@gmail.com>
2016-12-05 08:53:10 +0100

    Krisinek kész

---
+++ krisinek/krisinek/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace krisinek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.csere = new System.Windows.Forms.TextBox();
+            this.keres = new System.Windows.Forms.TextBox();
+            this.szoveg = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.csere);
+            this.groupBox1.Controls.Add(this.keres);
+            this.groupBox1.Location = new System.Drawing.Point(0, 213);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(760, 49);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Csere";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(199, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Erre";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ezt cseréljük";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(387, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Csere!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // csere
+            // 
+            this.csere.Location = new System.Drawing.Point(231, 19);
+            this.csere.Name = "csere";
+            this.csere.Size = new System.Drawing.Size(100, 20);
+            this.csere.TabIndex = 1;
+            // 
+            // keres
+            // 
+            this.keres.Location = new System.Drawing.Point(77, 16);
+            this.keres.Name = "keres";
+            this.keres.Size = new System.Drawing.Size(100, 20);
+            this.keres.TabIndex = 0;
+            // 
+            // szoveg
+            // 
+            this.szoveg.AcceptsReturn = true;
+            this.szoveg.AllowDrop = true;
+            this.szoveg.Location = new System.Drawing.Point(13, 35);
+            this.szoveg.Multiline = true;
+            this.szoveg.Name = "szoveg";
+            this.szoveg.Size = new System.Drawing.Size(449, 172);
+            this.szoveg.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(227, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Szöveg";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.szoveg);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox csere;
+        private System.Windows.Forms.TextBox keres;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox szoveg;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
