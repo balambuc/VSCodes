557f4225e75aab48042bde3de3a252cd2fbdc728
Bence Bardosi <bardosi.bence@gmail.com>
2016-12-07 12:42:00 +0100

    Gazsinak kész

---
+++ Gazsinak/Gazsinak/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gazsinak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Otos huzas = new Otos( );
+            Console.WriteLine(huzas.ToString( ));
+            Console.ReadLine( );
+        }
+    }
+
+    class Otos
+    {
+        private SortedSet<int> huzas = new SortedSet<int>();
+        private Random szam = new Random( );
+        private bool sorsolt = false;
+        public int[] sorsol()
+        {
+            while (huzas.Count<5)
+                huzas.Add(szam.Next(91));
+            sorsolt = true;
+            return this.huzas.ToArray();
+        }
+
+        public override string ToString()
+        {
+            if (!sorsolt)
+                this.sorsol( );
+            string s = "";
+            for (int i = 0; i < 4; i++)
+                s += huzas.ToArray()[i] + ", ";
+            
+            return s + huzas.ToArray()[4];
+        }
+        
+    }
+}
